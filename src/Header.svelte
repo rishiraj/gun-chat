d7659eb8f9c3d655e0cc616c73b9063087ec2538
Rishiraj Acharya <44090649+rishiraj@users.noreply.github.com>
2021-09-02 00:38:14 +0530

    Update Header.svelte

--- src/Header.svelte
+++ src/Header.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <header>
-<h1>🔫💬</h1>
+<h1>🧠</h1>
   {#if $username}
     <div class="user-bio">
       
@@ -20,6 +20,6 @@
 
     {:else}
 
-      <h3>Gun.js Chat</h3>
+      <h3>Deep Fusion AI Chat</h3>
   {/if}
 </header>
